da5b7c4a7f7e0c857981d03e75484e204d45cb72
ShivaTagh <127795324+ShivaTagh@users.noreply.github.com>
2023-08-22 21:34:53 -0700

    removed db from connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,12 +1,4 @@
 const mongoose = require('mongoose');
-const db = 'mongodb+srv://Shivatagh:Nikamir2018@@cluster0.fkihocw.mongodb.net/?retryWrites=true&w=majority'
-mongoose
-    .connect(db, { 
-        useNewUrlParser: true,
-        useCreateIndex: true
-      })
-    .then(() => console.log('MongoDB connected...'))
-    .catch(err => console.log(err));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
   useNewUrlParser: true,
